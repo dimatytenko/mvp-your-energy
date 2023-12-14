3a67ce089c277b50d2616a9f8f2eefb53061ba5c
Aleksey-Dr <aprel181818@gmail.com>
2023-12-14 15:45:53 +0200

    header#3 add styles and scroll-blocking
    header#3 add styles and scroll-blocking
    1. Changed button to link of page.
    2. Added styled for link of page.
    3. Added scroll-blocking when mobile-menu is open.

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,32 +1,23 @@
 (() => {
 	const mobileMenu = document.querySelector('.js-mobile-menu');
 	const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-close-menu');
+	const closeMenuBtn = document.querySelector('.js-close-menu');
+	const mobileSocialLinks = document.querySelectorAll('.mobile-social-box');
+	const body = document.querySelector('body');
 
 	const toggleMenu = () => {
 		const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
 		openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
 		mobileMenu.classList.toggle('is-open');
+		body.classList.toggle('disable-scroll');
 	};
 
-	openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
-    
     const closeMenuToClick = (elem) =>
         elem.forEach(link => {
 		link.addEventListener('click', toggleMenu);
     });
 
-    const mobileNavLinks = document.querySelectorAll('.mobile-navigation');
-    closeMenuToClick(mobileNavLinks);
-
-    const mobileSocialLinks = document.querySelectorAll('.mobile-social-box');
+	openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
     closeMenuToClick(mobileSocialLinks);
-    
-	window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-		if (!e.matches) return;
-		mobileMenu.classList.remove('is-open');
-		openMenuBtn.setAttribute('aria-expanded', false);
-		bodyScrollLock.enableBodyScroll(document.body);
-	});
 })();
