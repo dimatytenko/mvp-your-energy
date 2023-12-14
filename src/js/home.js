6f48e006ed3f872336105e738e8070597c73ee31
Dima Tytenko <amgpetronass@gmail.com>
2023-12-14 20:41:56 +0200

    update categories

--- src/js/home.js
+++ src/js/home.js
@@ -15,6 +15,10 @@ async function onCategoriesListClick(e) {
   if (e.target.nodeName !== 'LI') {
     return;
   }
+  refs.exercise.textContent = '';
+  refs.exerciseDecor.classList.add('visually-hidden');
+  refs.categoryError.classList.add('visually-hidden');
+
   const currentActiveItem = e.currentTarget.querySelector('.current');
   if (currentActiveItem) {
     currentActiveItem.classList.remove('current');
@@ -31,6 +35,18 @@ async function renderCategories() {
   const categories = await apiCategories.getCategories();
   getRenderCategories(categories.results, refs.categoriesContainer);
 
+  console.log('categories.totalPages <= 1', categories.totalPages <= 1);
+
+  if (!categories?.totalPages) {
+    refs.categoryError.classList.remove('visually-hidden');
+  }
+  if (categories.totalPages <= 1) {
+    console.log('categories.totalPages <= 1', categories.totalPages <= 1);
+    refs.paginationBox.classList.add('visually-hidden');
+    return;
+  }
+  refs.paginationBox.classList.remove('visually-hidden');
+
   pagination.init(apiCategories, categories.totalPages, categories.perPage);
 }
 renderCategories();
@@ -41,6 +57,8 @@ async function onCategoriesContainerClick(e) {
   console.log(e.target);
   if (e.target.closest('.js-exercise')) {
     const exercise = e.target.closest('.js-exercise').dataset.exercise;
+    refs.exercise.textContent = exercise;
+    refs.exerciseDecor.classList.remove('visually-hidden');
 
     await renderExercises(exercise);
     return;
@@ -53,6 +71,15 @@ async function renderExercises(exercise) {
   const exercises = await apiExercises.getExercises();
   console.log(exercises);
   getRenderExercises(exercises.results, refs.categoriesContainer);
+
+  if (!exercises?.totalPages) {
+    refs.categoryError.classList.remove('visually-hidden');
+  }
+  if (exercises.totalPages <= 1) {
+    refs.paginationBox.classList.add('visually-hidden');
+    return;
+  }
+  refs.paginationBox.classList.remove('visually-hidden');
 
   pagination.init(apiExercises, exercises.totalPages, exercises.perPage);
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -2,4 +2,7 @@ export const refs = {
   paginationBox: document.getElementById('tui-pagination'),
   categoriesContainer: document.querySelector('.js-categories-container'),
   categoriesList: document.querySelector('.js-categories-list'),
+  exercise: document.querySelector('.js-exercise'),
+  exerciseDecor: document.querySelector('.js-exercise-decor'),
+  categoryError: document.querySelector('.js-categories-error'),
 };
