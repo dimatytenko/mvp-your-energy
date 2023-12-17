224d71017efd2ff0d4d6eca1f6e21cba4133970a
vitalii-kyiv <131774628+vitalii-kyiv@users.noreply.github.com>
2023-12-17 22:22:18 +0000

    Deploying to gh-pages from @ dimatytenko/mvp-your-energy@664595e3de60fa793a4cbd2f7d7024b220325952 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{i as v}from"./assets/quote-17daa152.js";import"./assets/vendor-5014e6d0.js";const f={trashFavoritesBtn:document.querySelector(".js-exercises-trash-btn"),StartFavoritesBtn:document.querySelector(".js-exercises-start-btn"),exerciseFavorites:document.querySelector(".js-exercise-favorites"),categoryErrorFavorites:document.querySelector(".js-favorites-error")},u=f.exerciseFavorites,x=f.categoryErrorFavorites;console.log(f.exerciseFavorites);const a="savedExercises";g();function g(){const e=JSON.parse(localStorage.getItem(a))??[];if(e.length===0){console.log("savedExercis"),x.classList.remove("visually-hidden");return}console.log(e),u.innerHTML=m(e);const s=document.getElementsByClassName("favorite-card-item");for(let t=0;t<s.length;t+=1)console.log(s[t]),s[t].addEventListener("click",n),s[t].addEventListener("click",l);function n(t){const c=t.target.closest(".js-exercises-trash-btn"),o=t.target.closest(".card-item");if(c){const d=o.id;let r=e.findIndex(i=>i._id===`${d}`);if(console.log(r),console.log(e[r]),r>=0){for(let i=0;i<s.length;i+=1)s[i].removeEventListener("click",n),s[i].removeEventListener("click",l);e.splice(r,1),localStorage.removeItem(a),localStorage.setItem(a,JSON.stringify(e))}u.innerHTML=m(e),g()}}function l(t){const c=t.target.closest(".js-exercises-start-btn"),o=t.target.closest(".card-item");if(console.log(window.location.href),c){const d=o.id;let r=e.findIndex(i=>i._id===`${d}`);console.log(r),console.log(e[r]),r>=0&&(console.log(r),localStorage.removeItem(a),localStorage.setItem(a,JSON.stringify(e))),u.innerHTML=m(e),g()}}}function m(e){return e.map(({_id:s,target:n,bodyPart:l,burnedCalories:t,time:c,name:o})=>`
+import{i as v}from"./assets/quote-889d4774.js";import"./assets/vendor-5014e6d0.js";const f={trashFavoritesBtn:document.querySelector(".js-exercises-trash-btn"),StartFavoritesBtn:document.querySelector(".js-exercises-start-btn"),exerciseFavorites:document.querySelector(".js-exercise-favorites"),categoryErrorFavorites:document.querySelector(".js-favorites-error")},u=f.exerciseFavorites,x=f.categoryErrorFavorites;console.log(f.exerciseFavorites);const o="savedExercises";g();function g(){const e=JSON.parse(localStorage.getItem(o))??[];if(e.length===0){console.log("savedExercis"),x.classList.remove("visually-hidden");return}console.log(e),u.innerHTML=m(e);const s=document.getElementsByClassName("favorite-card-item");for(let t=0;t<s.length;t+=1)console.log(s[t]),s[t].addEventListener("click",n),s[t].addEventListener("click",l);function n(t){const i=t.target.closest(".js-exercises-trash-btn"),a=t.target.closest(".card-item");if(i){const d=a.id;let r=e.findIndex(c=>c._id===`${d}`);if(console.log(r),console.log(e[r]),r>=0){for(let c=0;c<s.length;c+=1)s[c].removeEventListener("click",n),s[c].removeEventListener("click",l);e.splice(r,1),localStorage.removeItem(o),localStorage.setItem(o,JSON.stringify(e))}u.innerHTML=m(e),g()}}function l(t){const i=t.target.closest(".js-exercises-start-btn"),a=t.target.closest(".card-item");if(console.log(window.location.href),i){const d=a.id;let r=e.findIndex(c=>c._id===`${d}`);console.log(r),console.log(e[r]),r>=0&&(console.log(r),localStorage.removeItem(o),localStorage.setItem(o,JSON.stringify(e))),u.innerHTML=m(e),g()}}}function m(e){return e.map(({_id:s,target:n,bodyPart:l,burnedCalories:t,time:i,name:a})=>`
             <li class="card-item favorite-card-item" id=${s}>
                 <div class="card-menu">
                 <div class="exercises-box">
@@ -38,22 +38,28 @@ import{i as v}from"./assets/quote-17daa152.js";import"./assets/vendor-5014e6d0.j
                      <use href="${v}#icon-run"></use>
                     </svg>
                 </div>
-                <p class="card-title-text">${o}</p>
+                <p class="card-title-text">${a}</p>
                 </div>
 
                 <div class="card-info">
-                    <div class="card-info-about">
-                        <p class="card-info-about-name">Burned calories:</p>
-                        <div class="card-info-about-value card-time">${t} / ${c} min</div>
-                    </div>
-                    <div class="card-info-about">
-                        <p class="card-info-about-name">Body part:</p>
-                        <div class="card-info-about-value card-body">${l}</div>
-                    </div>
-                    <div class="card-info-about">
-                        <p class="card-info-about-name">Target:</p>
-                        <div class="card-info-about-value card-target">${n}</div>
-                    </div>
+                <div class="gap-card">
+                    <p class="card-info-about-name">
+                    Burned calories:
+                    <div class="card-info-about-value card-time">${t} / ${i} min</div>
+                    </p>
+                </div>
+                <div class="gap-card">
+                    <p class="card-info-about-name">
+                    Body part:
+                    <div class="card-info-about-value card-body">${l}</div>
+                    </p>
+                </div>
+                <div class="gap-card">
+                    <p class="card-info-about-name">
+                    Target:
+                    <div class="card-info-about-value card-target">${n}</div>
+                    </p>
+                </div>
                 </div>
             </li>
         `).join("")}
